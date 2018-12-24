824538050e25183c5ee04bcd4eb7e6247d3a553f
mduccc <duc2521997@gmail.com>
2018-12-24 16:23:08 +0700

    Version 1.0.3

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Submit Code
  * Description:       Submit your code (I'm Tester)
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            IndieTeam
  * Author URI:
  * Text Domain:
